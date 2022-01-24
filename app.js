55d6c0d0cedae911ccc8b02c43afc0aceab403d2
Adam McCrea <adam@adamlogic.com>
2022-01-24 09:14:23 -0500

    Add homepage copy

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('index', {
+    judoscaleIsInstalled: process.env.JUDOSCALE_URL,
+  })
 })
 
 app.listen(port, () => {
